992ff74fa80559dcc753656a755ca7746982ee53
Ana Lara Garcia <analaraagarcia@hotmail.com>
2024-05-20 10:49:51 -0300

    adicao de novas solucoes

---
+++ solutions-beecrowd/beecrowd1186.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+
+    int i, j;
+    char O;
+    float M[12][12];
+    float soma=0;
+
+    scanf("%c", &O);
+
+    for (i=0; i<12; i++){
+        for (j=0; j<12; j++){
+
+            scanf("%f", &M[i][j]);
+
+        }
+    }
+
+    for (i=0; i<12; i++) {
+        for (j=0; j<12; j++) {
+            if (i + j > 11) {
+                soma += M[i][j];
+            }
+        }
+    }
+
+    if (O=='S'){
+        printf("%.1f\n", soma);
+    }else if (O=='M'){
+        printf("%.1f\n", soma/66.0);
+    }
+
+
+    return 0;
+}
